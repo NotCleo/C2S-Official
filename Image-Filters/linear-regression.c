a7738c67f3a190d50aefb25e7d4d166466edde44
Amruth Ayaan <144106562+NotCleo@users.noreply.github.com>
2026-02-08 00:58:48 +0530

    Update linear-regression.c

--- Image-Filters/linear-regression.c
+++ Image-Filters/linear-regression.c
@@ -8,20 +8,15 @@
 		Possibly use this code on the Arty7 board via HLS
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-
-#define n 5
-#define d 2
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h>
 
 void inversematrix(float matrix[d][d], float inverse[d][d]) {
     for (int i = 0; i < d; i++) {
         for (int j = 0; j < d; j++) {
-            if (i == j)
-                inverse[i][j] = 1.0;
-            else
-                inverse[i][j] = 0.0;
+            if (i == j) inverse[i][j] = 1.0;
+            else inverse[i][j] = 0.0;
         }
     }
     for (int i = 0; i < d; i++) {
@@ -31,20 +26,20 @@ void inversematrix(float matrix[d][d], float inverse[d][d]) {
                 pivot = j;
             }
         }
-        float temp[d];
+        
+        float temp[d]; 
         for (int k = 0; k < d; k++) {
             float t = matrix[i][k];
             matrix[i][k] = matrix[pivot][k];
             matrix[pivot][k] = t;
-        }
-        for (int k = 0; k < d; k++) {
-            float t = inverse[i][k];
+            
+            t = inverse[i][k];
             inverse[i][k] = inverse[pivot][k];
             inverse[pivot][k] = t;
         }
 
         if (fabs(matrix[i][i]) < 1e-9) {
-            printf("The X vector does not have a valid inverse");
+            printf("The matrix is singular\n");
             exit(1);
         }
 
@@ -67,75 +62,83 @@ void inversematrix(float matrix[d][d], float inverse[d][d]) {
 }
 
 int main(){
-	float X[n][d];
-	float X_transpose[d][n];
-	float y[n][1];
-	float w[d][1];
-	
-	for(int i=0; i<n; i++){
-		for(int j=0; j<d; j++){
-			X_transpose[j][i] = X[i][j];			
-				}
-			}
-	float X_transpose_X_product[d][d];
-	
-	// (rows,columns)
-	// (dxd) = (dxn) * (nxd) 
-	float sum = 0;
-	for(int i=0; i<d; i++){
-		for(int j=0; j<d; j++){
-			for(int k=0; k<n; k++){
-			sum += X_transpose[i][k] * X[k][j];						
-			}
-			X_transpose_X_product[i][j] = sum;
-			sum = 0;
-		}
-	}
-	
-	float X_transpose_y_product[d][1];
-	sum = 0;
-	// (rows,columns)
-	// (dx1) = (dxn) * (nx1) 
-	for(int i=0; i<d; i++){
-		for(int j=0; j<1; j++){
-			for(int k=0; k<n; k++){
-			sum += X_transpose[i][k] * y[k][j];						
-			}
-			X_transpose_y_product[i][j] = sum;
-			sum = 0;
-		}
-	}
-	
-	float X_transpose_X_product_inverse[d][d];
-	
-	inversematrix(X_transpose_X_product, X_transpose_X_product_inverse);
-	
-	
-	float X_transpose_X_product_inverse_X_transpose_y_product_product[d][1];
-	
-	// (dx1) = (dxd) * (dx1)
-	sum = 0;
-	for(int i=0; i<d; i++){
-		for(int j=0; j<1; j++){
-			for(int k=0; k<d; k++){
-			sum += X_transpose_X_product_inverse[i][k] * X_transpose_y_product[k][j];						
-			}
-			X_transpose_X_product_inverse_X_transpose_y_product_product[i][j] = sum;
-			sum = 0;
-		}
-	}	
-	
-	
-	for(int i=0; i<d; i++){
-		printf("%f ", X_transpose_X_product_inverse_X_transpose_y_product_product[i][0]);
-		}
+
+    // Column 0 is always 1
+    float X[n][d];
+    
+    float y[n][1];
+
+    float X_transpose[d][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<d; j++){
+            X_transpose[j][i] = X[i][j];            
+        }
+    }
+
+    float X_transpose_X_product[d][d];
+    float sum = 0; 
+    
+    for(int i=0; i<d; i++){
+        for(int j=0; j<d; j++){
+            sum = 0;
+            for(int k=0; k<n; k++){
+                sum += X_transpose[i][k] * X[k][j];                        
+            }
+            X_transpose_X_product[i][j] = sum;
+        }
+    }
+    
+    float X_transpose_y_product[d][1];
+    
+    for(int i=0; i<d; i++){
+        for(int j=0; j<1; j++){
+            sum = 0; 
+            for(int k=0; k<n; k++){
+                sum += X_transpose[i][k] * y[k][j];                        
+            }
+            X_transpose_y_product[i][j] = sum;
+        }
+    }
+    
+    float X_transpose_X_product_inverse[d][d];
+    inversematrix(X_transpose_X_product, X_transpose_X_product_inverse);
+    
+    float final_weights[d][1]; 
+    
+    for(int i=0; i<d; i++){
+        for(int j=0; j<1; j++){
+            sum = 0;
+            for(int k=0; k<d; k++){ 
+                sum += X_transpose_X_product_inverse[i][k] * X_transpose_y_product[k][j];                        
+            }
+            final_weights[i][j] = sum;
+        }
+    }    
+    
+    int choice;
+    float input_val;
+    float prediction;
+
+    while(1) {
+        printf("1. Predict\n");
+        printf("2. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("Enter value: ");
+            scanf("%f", &input_val);
+
+            prediction = final_weights[0][0] + (final_weights[1][0] * input_val);
+
+            printf("Predicted value: %.3f\n", prediction);
+        } 
+        else if (choice == 2) {
+            printf("ngl gang ts pmo\n");
+            break;
+        } 
+        else {
+            printf("Invalid\n");
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
